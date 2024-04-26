74f7e3808572a64288b0886f736ff084a39b353e
SCHADRAC WANZA ISULA <schadracw@hotmail.com>
2024-04-26 14:13:06 -0300

    Atualização

--- recipes/views.py
+++ recipes/views.py
@@ -6,10 +6,3 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'recipes/home.html')
 
-
-def contato(request):
-    return HttpResponse("contato")
-
-
-def sobre(request):
-    return HttpResponse("sobre")
