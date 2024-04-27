1dfd92dc0cbeffae3804a83ec03f8c1fb4af5d45
SCHADRAC WANZA ISULA <schadracw@hotmail.com>
2024-04-27 19:38:49 -0300

    atualização do urls

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'SCHADRAC W. ISULA',
+    })
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'SCHADRAC W. ISULA',
+    })
