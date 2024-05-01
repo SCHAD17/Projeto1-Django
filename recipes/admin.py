d107a01e9751d4ee749af22c9231e145a4f2dd48
SCHADRAC WANZA ISULA <schadracw@hotmail.com>
2024-04-30 23:09:16 -0300

    Adcionando Recipes na admin do django

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Recipe
+
 
 class CategoryAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.register(Category, CategoryAdmin)
