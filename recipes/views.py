ace338e439ad3b2b1f97bed48af3c6e55a5e9ed9
SCHADRAC WANZA ISULA <schadracw@hotmail.com>
2024-05-11 10:55:06 -0300

    corrigindo o erro da categoria

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe   
-from recipes.models import Recipe
+from .models import Recipe
 # Create your views here.
 
 def home(request):
